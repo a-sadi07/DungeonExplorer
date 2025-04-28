7b2c42f3e374486229d18282d6781c8de794739c
a-sadi07 <asifsadi07@gmail.com>
2025-04-28 07:08:06 -0700

    Update player.cs

--- player.cs
+++ player.cs
@@ -1,33 +1,149 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DungeonExplorer
 {
-    public class Player
+    public interface IDamageable
     {
-        public string Name { get; private set; }
-        public int Health { get; private set; }
-        private List<string> inventory;
+        void TakeDamage(int amount);
+    }
 
-        public Player(string name, int health)
+    public interface ICollectible
+    {
+        string Name { get; }
+        void Use(Player player);
+    }
+
+    public abstract class Creature : IDamageable
+    {
+        public string Name { get; protected set; }
+        public int Health { get; protected set; }
+
+        public Creature(string name, int health)
         {
             Name = name;
             Health = health;
-            inventory = new List<string>();
         }
 
-        public void PickUpItem(string item)
+        public virtual void TakeDamage(int amount)
         {
-            inventory.Add(item);
+            Health -= amount;
+            if (Health < 0) Health = 0;
+        }
+    }
+
+    public abstract class Monster : Creature
+    {
+        public int Damage { get; protected set; }
+
+        public Monster(string name, int health, int damage) : base(name, health)
+        {
+            Damage = damage;
+        }
+
+        public virtual void Attack(Player player)
+        {
+            Console.WriteLine($"{Name} attacks {player.Name} for {Damage} damage!");
+            player.TakeDamage(Damage);
+        }
+    }
+
+    public class Dragon : Monster
+    {
+        public Dragon(string name, int health, int damage) : base(name, health, damage) { }
+
+        public override void Attack(Player player)
+        {
+            Console.WriteLine($"{Name} breathes fire on {player.Name}!");
+            base.Attack(player);
+        }
+    }
+
+    public class Goblin : Monster
+    {
+        public Goblin(string name, int health, int damage) : base(name, health, damage) { }
+
+        public override void Attack(Player player)
+        {
+            Console.WriteLine($"{Name} slashes at {player.Name} with a dagger!");
+            base.Attack(player);
+        }
+    }
+
+    public class Weapon : ICollectible
+    {
+        public string Name { get; }
+        public int Damage { get; }
+
+        public Weapon(string name, int damage)
+        {
+            Name = name;
+            Damage = damage;
+        }
+
+        public void Use(Player player)
+        {
+            Console.WriteLine("You can't use a weapon like that directly.");
+        }
+    }
+
+    public class Potion : ICollectible
+    {
+        public string Name { get; }
+        public int HealAmount { get; }
+
+        public Potion(string name, int healAmount)
+        {
+            Name = name;
+            HealAmount = healAmount;
+        }
+
+        public void Use(Player player)
+        {
+            player.Heal(HealAmount);
+            Console.WriteLine($"You used a {Name} and healed {HealAmount} HP!");
+        }
+    }
+
+    public class Player : Creature
+    {
+        public List<ICollectible> Inventory { get; private set; }
+
+        public Player(string name, int health) : base(name, health)
+        {
+            Inventory = new List<ICollectible>();
         }
 
         public void DisplayStatus()
         {
-            Console.WriteLine($"Player: {Name}, Health: {Health}, Inventory: {InventoryContents()}");
+            Console.WriteLine($"Player: {Name} | Health: {Health} | Inventory: {string.Join(", ", Inventory.Select(i => i.Name))}");
         }
 
-        private string InventoryContents()
+        public void Heal(int amount)
         {
-            return inventory.Count > 0 ? string.Join(", ", inventory) : "Nothing";
+            Health += amount;
+        }
+
+        public void Attack(Monster monster)
+        {
+            Weapon weapon = Inventory.OfType<Weapon>().OrderByDescending(w => w.Damage).FirstOrDefault();
+            int damage = weapon?.Damage ?? 5;
+            Console.WriteLine($"You attack the {monster.Name} with {(weapon != null ? weapon.Name : "your fists")} for {damage} damage!");
+            monster.TakeDamage(damage);
+        }
+
+        public void UseItem(string itemName)
+        {
+            ICollectible item = Inventory.FirstOrDefault(i => i.Name.Equals(itemName, StringComparison.OrdinalIgnoreCase));
+            if (item == null)
+            {
+                Console.WriteLine("Item not found.");
+                return;
+            }
+            item.Use(this);
+            if (item is Potion) Inventory.Remove(item);
         }
     }
 }
+
